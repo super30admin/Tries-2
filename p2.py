2396412c3b6c5c72c8a740e0079a450b5e9515b8
Amrutha Prakash Veena <pvamrutha2509@gmail.com>
2021-01-26 10:39:59 -0500

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,37 @@
+#time: O(NM)
+#Space: O(1)
+
+class Solution:
+
+    def isMatch(self, query: str, pattern: str) -> bool:
+
+       
+        currentPatternIndex = 0
+
+        
+        for ch in query:
+
+           
+            if (currentPatternIndex < len(pattern) and char == pattern[currentPatternIndex]):
+                currentPatternIndex += 1
+
+           
+            elif (ch.isupper()):
+                return False
+
+            else:
+                continue
+
+       
+        return currentPatternIndex == len(pattern)
+
+    def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
+
+       
+        results = []
+
+        for query in queries:
+            results.append(self.isMatch(query, pattern))
+
+       
+        return results
